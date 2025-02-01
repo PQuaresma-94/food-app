48961ceab048aa6b5188412a192493816693a3df
PQuaresma-94 <pedromquaresma@gmail.com>
2025-01-31 17:59:56 -0800

    Create Yelp API request with axios

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,19 +1,29 @@
 import React, { useState } from "react";
 import { Text, StyleSheet, View } from "react-native";
 import SearchBar from "../components/SearchBar";
+import yelp from "../api/yelp";
 
 const SearchScreen = () => {
   const [term, setTerm] = useState("");
+  const [results, setResults] = useState([]);
+
+  const searchApi = async () => {
+    const response = await yelp.get("/search", {
+      params: {
+        limit: 50,
+        term,
+        location: "san jose",
+      },
+    });
+    setResults(response.data.businesses);
+  };
 
   return (
     <View style={styles.screen}>
-      <SearchBar
-        term={term}
-        onTermChange={(newTerm) => setTerm(newTerm)}
-        onTermSubmit={() => console.log("term was submitted")}
-      />
+      <SearchBar term={term} onTermChange={setTerm} onTermSubmit={searchApi} />
       <Text>Search Screen</Text>
-      <Text>{term}</Text>
+      <Text>Searching for: {term} </Text>
+      <Text>We have found {results.length} results</Text>
     </View>
   );
 };
